31aa4af4dc385deb7dc987ef6f5d0a359e1ec293
Thinuja Hettiarachchi <100994721+Thinuj01@users.noreply.github.com>
2025-11-24 12:18:01 +0530

    Renamed next.config.ts -> js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -11,7 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  
 };
 
-export default nextConfig;
+module.exports = nextConfig;
